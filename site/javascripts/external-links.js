05c916543b313929e0086c55ba8d72352aee840c
Ryan Panchadsaram <ryanpanchadsaram@gmail.com>
2013-02-09 17:13:35 -0500

    Detecting external non-.gov links and popping modal

---
+++ site/javascripts/external-links.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+ 
+  // Creating custom :external selector
+  $.expr[':'].external = function(obj){
+      return !obj.href.match(/^mailto\:/)
+              && !obj.href.match(/.gov/)
+              && (obj.hostname != location.hostname);
+  };
+ 
+  // Add 'external' CSS class to all external links
+  $('a:external').addClass('external');
+ 
+  $('.external').click(function() {
+    var link = $(this).attr('href');
+
+    $("#ExternalModal").reveal();
+    $("#external-link-text").text(link)
+    $("#external-link").attr("href", link)
+ 
+    return false;
+  });
+});
