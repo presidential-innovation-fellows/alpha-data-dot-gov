2f2b908be1dfd47699c01813e8f911325e2cd397
Ryan Panchadsaram <ryanpanchadsaram@gmail.com>
2013-02-12 10:21:53 -0500

    Manually Tagging External Links

--- site/javascripts/external-links.js
+++ site/javascripts/external-links.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
  
   // Creating custom :external selector
-  $.expr[':'].external = function(obj){
-      return !obj.href.match(/^mailto\:/)
-              && !obj.href.match(/.gov/)
-              && (obj.hostname != location.hostname);
-  };
+  // $.expr[':'].external = function(obj){
+  //     return !obj.href.match(/^mailto\:/)
+  //             && !obj.href.match(/.gov/)
+  //             && (obj.hostname != location.hostname);
+  // };
  
   // Add 'external' CSS class to all external links
-  $('a:external').addClass('external');
+  // $('a:external').addClass('external');
  
   $('.external').click(function() {
     var link = $(this).attr('href');
